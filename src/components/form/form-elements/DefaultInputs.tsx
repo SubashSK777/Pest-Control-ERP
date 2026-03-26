61686495cfc33f95f756d3afa153032115786969
SubashSK777 <subashsk11831@gmail.com>
2026-03-26 10:59:58 +0530

    fix: update icon references to match new premium naming convention

--- src/components/form/form-elements/DefaultInputs.tsx
+++ src/components/form/form-elements/DefaultInputs.tsx
@@ -4,7 +4,7 @@ import ComponentCard from '../../common/ComponentCard';
 import Label from '../Label';
 import Input from '../input/InputField';
 import Select from '../Select';
-import { ChevronDownIcon, EyeCloseIcon, EyeIcon, TimeIcon } from '../../../icons';
+import { ChevronDownIcon, EyeCloseIcon, EyeIcon, WatchIcon } from '../../../icons';
 import DatePicker from '@/components/form/date-picker';
 
 export default function DefaultInputs() {
@@ -84,7 +84,7 @@ export default function DefaultInputs() {
               onChange={(e) => console.log(e.target.value)}
             />
             <span className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
-              <TimeIcon />
+              <WatchIcon />
             </span>
           </div>
         </div>
